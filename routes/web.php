e71b85a9d1e2e25c404c692d269d20649e364516
Alessandro <marzucoalessandro1@gmail.com>
2021-03-01 17:56:05 +0100

    creazione del controller x admin

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('about', 'PageController@about');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+// Route x admin
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->group(function (){
+    Route::resource('posts', 'PostController');
+});
